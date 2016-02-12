528e9c4aa2be9baf43fa632079803ad3e61d8a67
Cristian Avalos <cavalos0086@gmail.com>
2016-02-11 19:16:37 -0500

    modified app to use angular states.Landing page is now render usisng stages

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,16 @@
-angular.module('pomoWebApp');
+angular.module('pomoWebApp',[
+	'ui.router',
+])
+	.config(function($stateProvider, $urlRouterProvider){
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider
+		.state('landing',{
+			url:'/',
+			views:{
+				'main':{
+					templateUrl:'app/components/landing/landing.html'
+				}
+			}
+		})
+	});
