591910b25a17a23f8234cbe7ff74034d9417864b
Cristian Avalos <cavalos0086@gmail.com>
2016-02-12 22:30:09 -0500

    Code Refactor. All services functions are going to be created inside a main factory

--- public/app/components/addTodo/addTodo.controller.js
+++ public/app/components/addTodo/addTodo.controller.js
@@ -1,15 +1,5 @@
 angular.module('pomoWebApp')
 	.controller('addTodoController', function($scope){
-		$scope.data = {
-			title:'',
-			description:'',
-			numPomo:0
-		};
-		$scope.numTodos = 0;
-		$scope.addTodo = function(){
-			if( ($scope.data.title !== '') && ($scope.data.numPomo > 0) && ($scope.data.numPomo <= 10)){
-				$scope.numTodos++;
-			}
-		};
+		
 
 	});
