aa74f464e00293dcad16758b7ed44ec6ba7d87d3
Cristian Avalos <cavalos0086@gmail.com>
2016-02-12 23:10:27 -0500

    created toDoItems table for storage, next: routing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/CristianAvalos';
+
 var bodyParser = require('body-parser');
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -10,13 +14,28 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 // app configuration
 app.use(express.static(__dirname+'/public'));
 app.use('/node_modules', express.static(__dirname +'/node_modules'));
-
-// application
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
-});
-
 var port = process.env.PORT || 8000;
 
 app.listen(port);
 console.log("App listening on",port);
+
+
+
+// application database
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+
+var client = new pg.Client(connectionString);
+client.connect();
+
+var query = client.query('DROP TABLE IF EXISTS toDoItems');
+var query1 = client.query('CREATE TABLE toDoItems(id SERIAL PRIMARY KEY, title varchar(200), description varchar(500), numPomodoros integer)');
+
+query1.on('end', function(){
+	client.end();
+});
+
+
