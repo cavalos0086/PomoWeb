93947bf4ea8eb8e494ebdc8bfb357fc5f7ceb328
Cristian Avalos <cavalos0086@gmail.com>
2016-02-13 19:51:10 -0500

    added "none added yet" when list is empty

--- public/app/components/todoList/todoList.controller.js
+++ public/app/components/todoList/todoList.controller.js
@@ -1,7 +1,6 @@
 angular.module('pomoWebApp')
 	.controller('todoListController', ['$scope', 'toDoFuntions','toDoItem',function($scope, toDoFuntions,toDoItem){
 		$scope.toDoList = [];
-
-		
+		$scope.empty = false;
 
 	}]);
