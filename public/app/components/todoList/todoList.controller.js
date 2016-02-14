11d78db45db1c5fcfd23b19da245957f96e86373
Cristian Avalos <cavalos0086@gmail.com>
2016-02-13 19:31:54 -0500

    todoList view done. next->switching to controller to ng-repeat data

---
+++ public/app/components/todoList/todoList.controller.js
@@ -0,0 +1,7 @@
+angular.module('pomoWebApp')
+	.controller('todoListController', ['$scope', 'toDoFuntions','toDoItem',function($scope, toDoFuntions,toDoItem){
+		$scope.toDoList = [];
+
+		
+
+	}]);
