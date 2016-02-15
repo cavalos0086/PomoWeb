c5590e905ccb0bc9fba963a5d27d9b8849d692a6
Cristian Avalos <cavalos0086@gmail.com>
2016-02-15 00:14:39 -0500

    created a basic clock timer

--- public/app/shared/modalWindow/timerModalCtrl.controller.js
+++ public/app/shared/modalWindow/timerModalCtrl.controller.js
@@ -2,18 +2,34 @@ angular.module('pomoWebApp')
 	.controller('timerModalCtrl', ['$scope','$uibModalInstance','toDo','toDoFunctions','$timeout', function($scope, $uibModalInstance,toDo, toDoFunctions, $timeout){
 
 		$scope.toDo = toDo;
-		var newData = new Date('February 14, 2016 11:40:40');
-		$scope.time = newData.getHours();
+		$scope.minutes = '10';
+		$scope.seconds = '00';
+
+
 
 		$scope.tickInterval = 1000; //ms
 
 		var tick = function(){
-			$scope.clock = Date.now();
+			$scope.temp = (parseInt($scope.seconds) - 1).toString();
+			if($scope.temp === '-1'){
+				$scope.minutes = (parseInt($scope.minutes) - 1).toString();
+				if(parseInt($scope.minutes) < 10){
+					$scope.minutes = '0' + $scope.minutes.toString();
+				}
+				$scope.seconds = '59';
+			} else{
+				$scope.seconds = (parseInt($scope.seconds) - 1).toString();
+				if(parseInt($scope.seconds) < 10){
+					$scope.seconds = '0' + $scope.seconds.toString();
+				}
+			}
+
+
 			$timeout(tick, $scope.tickInterval);
 		}
 
 		$scope.start = function(){
-			$timeout(tick, $scope.tickInterval);
+			tick();
 		};
 
 		$scope.cancel = function(){
